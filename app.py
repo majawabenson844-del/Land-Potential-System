20efa3a983c07c8e5f0a2ecf5b0dfd5897e02328
majawabenson844-del <majawabenson844@gmail.com>
2026-01-11 20:39:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import joblib
 # Page Config
 # ===============================
 st.set_page_config(
-    page_title="Benson Land System",
+    page_title="Benson's Groundwater Potential Mapping System",
     page_icon="🌍",
     layout="wide",
     initial_sidebar_state="expanded"
